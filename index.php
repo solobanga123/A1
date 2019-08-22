878762c3e3c8955854b6826bafa6e13e6b825ece
solobanga123 <54232596+solobanga123@users.noreply.github.com>
2019-08-22 08:13:22 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World from Netbeans</p>'; 
-    putenv("DATABASE_URL=fred");
-    echo getenv("DATABASE_URL")
- ?> 
-     
- </body>
-</html>
+<?php include_once("home.html"); ?>
